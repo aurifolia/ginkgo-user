7f4f5f1818dfe05ba6e9da73df2e359cbb7339cf
Peng Dan <vnelinpe@sina.com>
2025-11-10 17:40:39 +0800

    init

---
+++ src/main/java/org/aurifolia/cloud/ginkgo/user/GinkgoUserApplication.java
@@ -0,0 +1,18 @@
+package org.aurifolia.cloud.ginkgo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GinkgoUserApplication
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+@SpringBootApplication
+public class GinkgoUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinkgoUserApplication.class, args);
+	}
+}
---
+++ src/test/java/org/aurifolia/cloud/ginkgo/user/GinkgoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aurifolia.cloud.ginkgo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinkgoUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
