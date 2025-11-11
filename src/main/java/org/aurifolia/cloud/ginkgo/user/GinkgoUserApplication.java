953074d35f583564ac55422b5a40672c87c2f490
Peng Dan <vnelinpe@sina.com>
2025-11-11 18:00:47 +0800

    init

--- src/main/java/org/aurifolia/cloud/ginkgo/user/GinkgoUserApplication.java
+++ src/main/java/org/aurifolia/cloud/ginkgo/user/GinkgoUserApplication.java
@@ -1,7 +1,13 @@
 package org.aurifolia.cloud.ginkgo.user;
 
+import org.aurifolia.cloud.id.client.generator.IdGenerator;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * GinkgoUserApplication
@@ -15,4 +21,21 @@ public class GinkgoUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GinkgoUserApplication.class, args);
 	}
+
+    @Bean
+    public ApplicationRunner applicationRunner(IdGenerator idGenerator) {
+        return args -> {
+            int times = 10000000;
+            long id = -1;
+            Instant now = Instant.now();
+            for (int i = 0; i < times; i++) {
+                long current = idGenerator.nextId();
+                if (current < id) {
+                    throw new RuntimeException("id is duplicated");
+                }
+                id = current;
+            }
+            System.out.println((double) times / Duration.between(now, Instant.now()).toMillis());
+        };
+    }
 }
